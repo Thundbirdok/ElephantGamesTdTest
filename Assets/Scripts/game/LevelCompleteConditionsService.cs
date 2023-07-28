28e2ae5222dab1bdcc0c3a3655ee43218baf4f9c
VyacheslavGorokhov <vyacheslavgorohov@mail.ru>
2023-07-28 12:59:27 +0700

    Add animated sprites and sprite button to level_2

--- Assets/Scripts/game/LevelCompleteConditionsService.cs
+++ Assets/Scripts/game/LevelCompleteConditionsService.cs
@@ -7,14 +7,14 @@ namespace game
     public class LevelCompleteConditionsService : MonoBehaviour
     {
         [SerializeField]
-        private SpriteButton finishLevel;
+        private SpriteButton completeLevel;
 
         [SerializeField]
         private LevelStateHandler levelStateHandler;
         
-        private void OnEnable() => finishLevel.OnClick += CompleteLevel;
+        private void OnEnable() => completeLevel.OnClick += CompleteLevel;
 
-        private void OnDisable() => finishLevel.OnClick -= CompleteLevel;
+        private void OnDisable() => completeLevel.OnClick -= CompleteLevel;
 
         private void CompleteLevel() => levelStateHandler.CompleteLevel();
     }
